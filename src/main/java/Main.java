5754140a3a42f72c831754c56a82eaa691da4f2b
Corinne <corinne@Zipcoders-MacBook-Pro.local>
2023-10-26 17:30:44 -0400

    Added else if statement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
 
-    }
+        // Get name from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        //Check to see if it is 'Bob' or 'Alice
+        //Greet if name = 'Bob' or 'Alice'
+        if(name.equals("Alice") || name.equals("Bob")) {
+            System.out.println("Hey Bob or Alice");
+        }else{
+            System.out.println("Not Bob or Alice");
+        }
+    };
+
 }
